ffc4f956cbeceb2189bf25fc4c0288786809d3cd
jabuebag <ybagyang@gmail.com>
2016-10-01 11:47:33 -0700

    first commit

---
+++ src/main/java/com/jabue/model/Greeting.java
@@ -0,0 +1,23 @@
+package com.jabue.model;
+
+/**
+ * Created by Kris on 2016-10-01.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
